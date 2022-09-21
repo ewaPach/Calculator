38b835551b6a509ef0309d51c53216151044f5af
ewpac <ew.pach@gmail.com>
2022-09-21 20:54:09 +0200

    Added all mathematical methods

--- src/Main.java
+++ src/Main.java
@@ -2,25 +2,3 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter first value");
-        int x = scanner.nextInt();
-        System.out.println("Enter second value");
-        int y = scanner.nextInt();
-        System.out.println("Hello, what do you like to do?\n1.Addition\n2.Sunstraction\n3.Multiplication\n4.Division");
-        int select = scanner.nextInt();
-        switch (select){
-            case 1:
-                System.out.println("RESULT: "+(x+y));
-                break;
-            case 2:
-                System.out.println("RESULT: " +(x-y));
-                break;
-            case 3:
-                System.out.println("RESULT: " +(x*y));
-                break;
-            case 4:
-                System.out.println("RESULT: " +(x/y));
-                break;
-        }
-    }}
