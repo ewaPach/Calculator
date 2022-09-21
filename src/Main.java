c9ab32a0cb5b0e3946aa7d73fe64d1787ae632f0
ewpac <ew.pach@gmail.com>
2022-09-21 18:24:51 +0200

    Added mathematical operations.

--- src/Main.java
+++ src/Main.java
@@ -11,16 +11,16 @@ public class Main {
         int select = scanner.nextInt();
         switch (select){
             case 1:
-                System.out.println("Result: "+(x+y));
+                System.out.println("RESULT: "+(x+y));
                 break;
             case 2:
-                System.out.println("Result: " +(x-y));
+                System.out.println("RESULT: " +(x-y));
                 break;
             case 3:
-                System.out.println("Result: " +(x*y));
+                System.out.println("RESULT: " +(x*y));
                 break;
             case 4:
-                System.out.println("Result: " +(x/y));
+                System.out.println("RESULT: " +(x/y));
                 break;
         }
     }}
